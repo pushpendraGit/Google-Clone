0f7b3a060feaf04b2810a5bd57bbb16ee8d52650
Pushpendra Singh <pushpendra7060@gmail.com>
2020-08-21 00:26:56 +0530

    home is created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Googole.com 🚀</h1>
+      {/* Home */}
+      <Home />
+      {/* search page */}
+
+
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Home.css'
+
+function Home(props) {
+    return (
+        <div className="home">
+            <h1>This is a home page</h1>
+            <div className='home__header'>
+
+            </div>
+            <div className="home__body">
+
+                
+            </div>
+            
+        </div>
+    );
+}
+
+export default Home;
